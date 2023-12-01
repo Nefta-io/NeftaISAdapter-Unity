1e9a1fc540b114986d9bdf287a9609730b7afb2f
Tomaz Treven <treven@nefta.io>
2023-12-01 09:31:54 +0100

    Updated bundled NeftaSDK to latest (added progress bar to video placements)

--- Assets/NeftaCustomAdapter/Editor/NeftaIronSourcePostProcessor.cs
+++ Assets/NeftaCustomAdapter/Editor/NeftaIronSourcePostProcessor.cs
@@ -27,7 +27,7 @@ end";
                 var text = File.ReadAllText(path);
                 var podIndex = text.IndexOf("pod 'IronSourceSDK'", StringComparison.InvariantCulture);
                 var index = text.IndexOf('\n', podIndex); 
-                text = text.Insert(index + 1, $"  pod 'NeftaISAdapter', :git => 'https://github.com/Nefta-io/NeftaISAdapter.git', :tag => '{Application.version}'\n");
+                text = text.Insert(index + 1, "  pod 'NeftaISAdapter', :git => 'https://github.com/Nefta-io/NeftaISAdapter.git', :tag => '1.1.3'\n");
 
                 // if you're are in framework mode (use_frameworks!) and link them statically (:linkage => :static)
                 // var iphoneTargetIndex = text.IndexOf("target 'Unity-iPhone' do", StringComparison.InvariantCulture);
@@ -42,17 +42,17 @@ end";
         [MenuItem("Ads Mediation/Export Nefta Custom Adapter SDK", false, int.MaxValue)]
         private static void ExportAdSdkPackage()
         {
-          var packageName = $"NeftaIS_SDK_v{Application.version}.unitypackage";
+          var packageName = "NeftaIS_SDK_v1.1.3.unitypackage";
           var assetPaths = new string[] { "Assets/NeftaCustomAdapter" };
 
           try
           {
-            AssetDatabase.ExportPackage(assetPaths, packageName, ExportPackageOptions.Recurse);
-            Debug.Log($"Finished exporting {packageName}");   
+              AssetDatabase.ExportPackage(assetPaths, packageName, ExportPackageOptions.Recurse);
+              Debug.Log($"Finished exporting {packageName}");   
           }
           catch (Exception e)
           {
-            Debug.LogError($"Error exporting {packageName}: {e.Message}");   
+              Debug.LogError($"Error exporting {packageName}: {e.Message}");   
           }
         }
     }
