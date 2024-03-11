bf6b75d1e8f955929e4eced5d37112e112de241b
Tomaz Treven <treven@nefta.io>
2024-03-11 10:45:05 +0100

    Added support for muting rewarded video and getting of nuid.

--- Assets/NeftaCustomAdapter/Editor/NeftaIronSourcePostProcessor.cs
+++ Assets/NeftaCustomAdapter/Editor/NeftaIronSourcePostProcessor.cs
@@ -23,7 +23,7 @@ end";
         {
             if (target == BuildTarget.iOS)
             {
-                const string dependency = "pod 'NeftaISAdapter', :git => 'https://github.com/Nefta-io/NeftaISAdapter.git', :tag => '1.2.3'";
+                const string dependency = "pod 'NeftaISAdapter', :git => 'https://github.com/Nefta-io/NeftaISAdapter.git', tag => '1.2.4'";
                 
                 var path = buildPath + "/Podfile";
                 var text = File.ReadAllText(path);
--- Assets/NeftaCustomAdapter/Plugins/iOS/NeftaAdapter.m
+++ Assets/NeftaCustomAdapter/Plugins/iOS/NeftaAdapter.m
@@ -15,6 +15,7 @@ extern "C" {
     void EnableLogging(bool enable);
     void * NeftaPlugin_Init(const char *appId);
     void NeftaPlugin_Record(void *instance, const char *event);
+    const char * NeftaPlugin_ShowNuid(void *instance);
 #ifdef __cplusplus
 }
 #endif
@@ -25,13 +26,18 @@ void NeftaPlugin_EnableLogging(bool enable) {
     [NeftaPlugin_iOS EnableLogging: enable];
 }
 
-void * NeftaPlugin_Init(const char *appId)
-{
+void * NeftaPlugin_Init(const char *appId) {
     _plugin = [NeftaPlugin_iOS InitWithAppId: [NSString stringWithUTF8String: appId]];
     return (__bridge_retained void *)_plugin;
 }
 
-void NeftaPlugin_Record(void *instance, const char *event)
-{
+void NeftaPlugin_Record(void *instance, const char *event) {
     [_plugin RecordWithEvent: [NSString stringWithUTF8String: event]];
+}
+
+const char * NeftaPlugin_ShowNuid(void *instance) {
+    const char *string = [[_plugin ShowNuid] UTF8String];
+    char *returnString = (char *)malloc(strlen(string) + 1);
+    strcpy(returnString, string);
+    return returnString;
 }
